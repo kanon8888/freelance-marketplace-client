da759537265479eb94246de8ec6713667df09613
kanon8888 <mdrakibhasankanon@gmail.com>
2025-11-10 01:58:44 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RoodLayout from './layout/RoodLayout.jsx';
+import Home from './components/Home/Home.jsx';
+import AllJobs from './components/AllJobs/AllJobs.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RoodLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'allJobs',
+        Component: AllJobs
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />,
+  </StrictMode>,
+)
